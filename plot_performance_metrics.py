b2efb8dc032cce078ffa6fe64177fdcca89693b3
dsheets1242 <48427197+dsheets1242@users.noreply.github.com>
2023-05-25 13:23:38 -0500

    Added plotting functions and resultant figures

--- plot_performance_metrics.py
+++ plot_performance_metrics.py
@@ -39,6 +39,53 @@ def import_csv(filename):
                 values[idof].append(v)
     return column_names, values
 
+
+#%% plot confusion matrices
+
+def plot_conf_mat(audio, mfcc, axis_labels, data_sets, figure_path):
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(audio)
+    plt.colorbar(fraction=0.05)
+    plt.xticks(ticks=np.arange(0,len(axis_labels)), labels=axis_labels)
+    plt.yticks(ticks=np.arange(0,len(axis_labels)), labels=axis_labels)
+    plt.title('Audio Padding')
+    
+    plt.subplot(1,2,2)
+    plt.imshow(mfcc)
+    plt.colorbar(fraction=0.05)
+    plt.xticks(ticks=np.arange(0,len(axis_labels)), labels=axis_labels)
+    plt.yticks(ticks=np.arange(0,len(axis_labels)), labels=axis_labels)
+    plt.title('MFCC Padding')
+    
+    plt.suptitle(data_sets, y=0.82, fontweight='bold')
+    
+    plt.subplots_adjust(left=0.1,
+                        bottom=0.1,
+                        right=0.9,
+                        top=0.9,
+                        wspace=0.6,
+                        hspace=0.4)
+    plt.savefig(os.path.join(figure_path, '{}.png'.format(data_sets)),
+                dpi=300, bbox_inches='tight')
+    plt.close()
+    
+
+#%% plot model accuracy
+
+def plot_accuracy(audio, mfcc, test_dataset, title, figure_path, save_name):
+    plt.figure()
+    plt.plot(audio, linewidth=2, color='C0')
+    plt.plot(mfcc, linewidth=2, color='C1')
+    plt.xticks(ticks=np.arange(0,len(test_dataset)), labels=test_dataset)
+    plt.xlabel('Test Dataset')
+    plt.ylabel('Accuracy (%)')
+    plt.legend(['Audio Padding', 'MFCC Padding'])
+    plt.title(title, fontweight='bold')
+    plt.savefig(os.path.join(figure_path, '{}.png'.format(save_name)),
+                dpi=300, bbox_inches='tight')
+    plt.close()
+
 #%% set up path info and load data metrics
 
 data_types = ['audio', 'mfcc']
@@ -50,10 +97,6 @@ cur_dir = os.getcwd()
 results_path = os.path.join(cur_dir, results_dir)
 
 # load specified metrics
-conf_mat_audio = []
-eval_met_audio = []
-conf_mat_mfcc = []
-eval_met_mfcc = []
 for file in os.listdir(results_path):
     f = file.split('_')
     metric = '_'.join(f[0:2])
@@ -63,35 +106,99 @@ for file in os.listdir(results_path):
     if metric == 'CONF_MATRIX' or metric == 'EVAL_METRICS':
         if metric == 'CONF_MATRIX':
             if dtype == 'audio':
-                locals()['_'.join([metric, train_set, test_set, dtype, 'cols']).lower()],\
-                locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower()]\
+                cols_cm, locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')]\
                 = import_csv(os.path.join(results_path, file))            
-                conf_mat_audio.append(locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower()])
+                
+                locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')]\
+                    = np.array(locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')][1:]).T / \
+                        np.sum(np.array(locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')][1:]).T)
             elif dtype == 'mfcc':
-                locals()['_'.join([metric, train_set, test_set, dtype, 'cols']).lower()],\
-                locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower()]\
+                cols_cm, locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')]\
                 = import_csv(os.path.join(results_path, file))            
-                conf_mat_mfcc.append(locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower()])
+                
+                locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')]\
+                    = np.array(locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')][1:]).T / \
+                        np.sum(np.array(locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')][1:]).T)
         elif metric =='EVAL_METRICS':
             if dtype == 'audio':
-                locals()['_'.join([metric, train_set, test_set, dtype, 'cols']).lower()],\
-                locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower()]\
+                cols_em, locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')]\
                 = import_csv(os.path.join(results_path, file))            
-                eval_met_audio.append(locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower()])
+                
+                locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')]\
+                    = np.array(locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')][1:]).T
+                    
+                locals()['_'.join(['acc', train_set, test_set, dtype, 'vals']).lower().replace('-', '')] = \
+                    locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')][5,0]
             elif dtype == 'mfcc':
-                locals()['_'.join([metric, train_set, test_set, dtype, 'cols']).lower()],\
-                locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower()]\
+                cols_em, locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')]\
                 = import_csv(os.path.join(results_path, file))            
-                eval_met_mfcc.append(locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower()])
+                
+                locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')]\
+                    = np.array(locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')][1:]).T
+                    
+                locals()['_'.join(['acc', train_set, test_set, dtype, 'vals']).lower().replace('-', '')] = \
+                    locals()['_'.join([metric, train_set, test_set, dtype, 'vals']).lower().replace('-', '')][5,0]
         
 
+#%% generate confusion matrice plots
+
+figures_dir = 'Figures'
+conf_mat_dir = 'ConfMat'
+conf_mat_path = os.path.join(cur_dir, figures_dir, conf_mat_dir)
+
+# train 3, test 1
+plot_conf_mat(conf_matrix_emodbemovoravde_savee_audio_vals, conf_matrix_emodbemovoravde_savee_mfcc_vals,\
+              cols_cm[1:], 'EMODB-EMOVO-RAVDE_SAVEE', conf_mat_path)
+    
+plot_conf_mat(conf_matrix_emodbemovosavee_ravde_audio_vals, conf_matrix_emodbemovosavee_ravde_mfcc_vals,\
+              cols_cm[1:], 'EMODB-EMOVO-SAVEE_RAVDE', conf_mat_path)
+    
+plot_conf_mat(conf_matrix_emodbravdesavee_emovo_audio_vals, conf_matrix_emodbravdesavee_emovo_mfcc_vals,\
+              cols_cm[1:], 'EMODB-RAVDE-SAVEE_EMOVO', conf_mat_path)
+    
+plot_conf_mat(conf_matrix_emovoravdesavee_emodb_audio_vals, conf_matrix_emovoravdesavee_emodb_mfcc_vals,\
+              cols_cm[1:], 'EMOVO-RAVDE-SAVEE_EMODB', conf_mat_path)
+
+# language split
+plot_conf_mat(conf_matrix_emodbemovo_ravdesavee_audio_vals, conf_matrix_emodbemovo_ravdesavee_mfcc_vals,\
+              cols_cm[1:], 'EMODB-EMOVO_RAVDE-SAVEE', conf_mat_path)
+    
+plot_conf_mat(conf_matrix_ravdesavee_emodbemovo_audio_vals, conf_matrix_ravdesavee_emodbemovo_mfcc_vals,\
+              cols_cm[1:], 'RAVDE-SAVEE_EMODB-EMOVO', conf_mat_path)    
         
         
-        
-        
-        
-        
-        
+#%% generate accuracy plots
+
+acc_dir = 'Accuracy'
+acc_path = os.path.join(cur_dir, figures_dir, acc_dir)
+
+# train 3, test 1
+train3_test1_acc_audio = [acc_emodbemovoravde_savee_audio_vals*100,\
+                          acc_emodbemovosavee_ravde_audio_vals*100,\
+                          acc_emodbravdesavee_emovo_audio_vals*100,\
+                          acc_emovoravdesavee_emodb_audio_vals*100]
+train3_test1_acc_mfcc = [acc_emodbemovoravde_savee_mfcc_vals*100,\
+                          acc_emodbemovosavee_ravde_mfcc_vals*100,\
+                          acc_emodbravdesavee_emovo_mfcc_vals*100,\
+                          acc_emovoravdesavee_emodb_mfcc_vals*100]
+    
+train3_test1_title = 'Train on 3 Datasets, Test on 1 Dataset'
+train3_test1_test_datasets = ['SAVEE', 'RAVDE', 'EMOVO', 'EMODB']
+
+plot_accuracy(train3_test1_acc_audio, train3_test1_acc_mfcc, train3_test1_test_datasets, train3_test1_title, acc_path, 'Train3Test1')
+
+
+
+# language split
+lang_acc_audio = [acc_emodbemovo_ravdesavee_audio_vals*100,\
+                    acc_ravdesavee_emodbemovo_audio_vals*100]
+lang_acc_mfcc = [acc_emodbemovo_ravdesavee_mfcc_vals*100,\
+                    acc_ravdesavee_emodbemovo_mfcc_vals*100]
+    
+lang_title = 'Train/Test on Different Language Datasets'
+lang_test_datasets = ['SAVEE-RAVDE (Chinese/German/Italian)', 'EMOVO-EMODB (English)']
+
+plot_accuracy(lang_acc_audio, lang_acc_mfcc, lang_test_datasets, lang_title, acc_path, 'Language')
         
         
         
